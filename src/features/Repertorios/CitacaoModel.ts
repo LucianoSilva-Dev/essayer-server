b101e7b3114fd98c97104eed48266bfd23128d3d
MurilloFonseca <146749127+MurilloFonseca@users.noreply.github.com>
2025-04-27 19:49:30 -0300

    Feature/database (#7)
    * feature(database) adicionado os models do banco da versão 1.1
    
    * fix(database) adicionado o atributo 'subtopicos' às entidades do repertório
    
    * fix(database) mudado o nome dos arquivos com os models

---
+++ src/features/Repertorios/CitacaoModel.ts
@@ -0,0 +1,16 @@
+import { Schema, model } from 'mongoose';
+
+const CitacaoSchema = new Schema({
+    frase: {type: String, required: true},
+    autor: {type: String, required: true},
+    fonte: String,
+    criador: {type: Schema.Types.ObjectId, required: true, ref: 'Usuario'},
+    likes: [{type: Schema.Types.ObjectId, ref: 'Usuario'}],
+    comentarios: [{
+        usuario: {type: Schema.Types.ObjectId, required: true, ref: 'Usuario'}, 
+        texto: {type: String, required: true}
+    }],
+    subtopicos: [String]
+}, {timestamps: true})
+
+export const CitacaoModel = model('Citacao', CitacaoSchema);
---
+++ src/features/Repertorios/ObraModel.ts
@@ -0,0 +1,16 @@
+import { Schema, model } from 'mongoose';
+
+const ObraSchema = new Schema({
+    titulo: {type: String, required: true},
+    sinopse: {type: String, required: true},
+    autor: {type: String, required: true},
+    criador: {type: Schema.Types.ObjectId, required: true, ref: 'Usuario'},
+    likes: [{type: Schema.Types.ObjectId, ref: 'Usuario'}],
+    comentarios: [{
+        usuario: {type: Schema.Types.ObjectId, required: true, ref: 'Usuario'}, 
+        texto: {type: String, required: true}
+    }],
+    subtopicos: [String]
+}, {timestamps: true})
+
+export const ObraModel = model('Obra', ObraSchema);
