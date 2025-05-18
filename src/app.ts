eb225bfb1fbe00f16f70a266be38e5b5a208898e
LucianoSilva-Dev <lucianonascimento2626@gmail.com>
2025-05-18 12:25:02 -0300

    refactor: feature "background" substituida por "Repertorio" removida

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,6 @@ import fastifyMultipart from '@fastify/multipart';
 import { appErrorHandler } from './shared/plugins/appErrorHandler';
 
 import { AuthRoutes } from './features/Auth/Routes';
-import { BackgroundRoutes } from './features/Background/Routes';
 import { RepertorioRoutes } from './features/Repertorios/Routes';
 import { appConfig } from './config/app';
 import { RequisicaoProfessorRoutes, UsuarioRoutes } from './shared/Routes';
@@ -45,7 +44,6 @@ class App {
 
   private routes() {
     this.app.register(AuthRoutes, { prefix: '/auth' });
-    this.app.register(BackgroundRoutes, { prefix: '/background' });
     this.app.register(UsuarioRoutes, { prefix: '/usuario' });
     this.app.register(RequisicaoProfessorRoutes, {
       prefix: '/requisicao-professor',
