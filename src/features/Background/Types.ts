f6bdfd67abab503a3609ee01a1a4ab63a35b8d7f
LucianoSilva-Dev <lucianonascimento2626@gmail.com>
2025-06-24 20:48:51 -0300

    correções para o deploy baseado na branch test-deploy

--- src/features/Background/Types.ts
+++
@@ -1,6 +0,0 @@
-import type z from 'zod';
-import type { createBackgroundBodyValidation } from './Validations';
-import type { getBackgroundResponse } from './Schemas';
-
-export type createBackgroundBody = z.infer<typeof createBackgroundBodyValidation>;
-export type getBackgroundResponseType = z.infer<typeof getBackgroundResponse>;
