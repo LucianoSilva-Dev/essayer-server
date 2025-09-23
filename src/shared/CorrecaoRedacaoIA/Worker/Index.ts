3ec0a145e484a9d7eb8da40d1e4a1ceca2b6404e
LucianoSilva-Dev <lucianonascimento2626@gmail.com>
2025-09-23 16:17:16 -0300

    lógica de retry de jobs customizada

--- src/shared/CorrecaoRedacaoIA/Worker/Index.ts
+++ src/shared/CorrecaoRedacaoIA/Worker/Index.ts
@@ -2,11 +2,18 @@ import { Worker } from 'bullmq';
 import { redisClient } from '../../Redis/Provider';
 import type { AppJobMap } from '../../BullMQ/Types';
 import { handleJob } from './HandleJob';
+import { customBackoffStrategy } from './BackoffStrategy';
 
 export const correcaoRedacaoWorker = new Worker<AppJobMap['redacao:corrigir']>(
   'Redacao',
   handleJob,
-  { connection: redisClient, autorun: false },
+  {
+    connection: redisClient,
+    autorun: false,
+    settings: {
+      backoffStrategy: customBackoffStrategy,
+    },
+  },
 );
 
 correcaoRedacaoWorker.on('failed', (job, err) => {
