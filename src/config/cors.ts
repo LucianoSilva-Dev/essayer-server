99f210636a7fdb26d0c3ee5044e56ed49636e347
LucianoSilva-Dev <lucianonascimento2626@gmail.com>
2025-10-29 14:28:14 -0300

    chore(cors): mudanças no cors para compatibilidade com o frontend
    Verificações de compatibilidade do CORS com o frontend ainda necessárias

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,17 +1,22 @@
-import type { FastifyCorsOptions } from "@fastify/cors";
+import type { FastifyCorsOptions } from '@fastify/cors';
 
 export const corsConfig: FastifyCorsOptions = {
-  origin: '*',
+  origin: [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'http://[::1]:3001',
+    'https://incita.vercel.app'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  credentials: true,
   optionsSuccessStatus: 204,
   preflightContinue: false,
   allowedHeaders: [
     'content-type',
     'accept',
-    'content-type',
     'authorization',
     'cache-control',
     'expires',
-    'pragma'
+    'pragma',
   ],
 };
